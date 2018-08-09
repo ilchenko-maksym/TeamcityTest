cc43fca748febbf930a529a3f3ce003f664b7b22
Maksym Ilchenko <ilchenko.maksym@easypay.ua>
2018-08-09 12:14:13 +0300

    add calculate + test

--- Test/ConsoleApp1/Program.cs
+++ Test/ConsoleApp1/Program.cs
@@ -16,4 +16,41 @@ namespace ConsoleApp1
             Console.ReadKey();
         }
     }
+
+    public class CalCulator
+    {
+        // Square function   
+        public int Square(int num)
+        {
+            return num * num;
+        }
+        // Add two integers and returns the sum  
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Multiply two integers and retuns the result  
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        // Subtracts small number from big number  
+        public int Subtract(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+
+            return num2 - num1;
+
+        }
+        //performing Division on two float variables.  
+        public float Division(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+    }
 }
---
+++ Test/ConsoleApp1Tests/CalCulatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tests
+{
+    [TestClass()]
+    public class CalCulatorTests
+    {
+        [TestMethod()]
+        public void SquareTest()
+        {
+            var r = new CalCulator().Add(10, 10);
+            Assert.AreEqual(r, 20);
+        }
+    }
+}
